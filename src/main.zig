bb7319a6c5134f5d8bdd8ce91ac1af1d103340e1
Rylee Alanza Lyman <46907231+ryleelyman@users.noreply.github.com>
2023-06-29 17:30:53 -0400

    chore: bump version

--- src/main.zig
+++ src/main.zig
@@ -10,7 +10,7 @@ const input = @import("input.zig");
 const screen = @import("screen.zig");
 const midi = @import("midi.zig");
 
-const VERSION = .{ .major = 0, .minor = 12, .patch = 3 };
+const VERSION = .{ .major = 0, .minor = 12, .patch = 4 };
 
 pub const std_options = struct {
     pub const log_level = .info;
