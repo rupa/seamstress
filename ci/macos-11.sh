6fdb0ad6603b176c94037f3899ef75b8bea4df08
Rylee Alanza Lyman <46907231+ryleelyman@users.noreply.github.com>
2023-08-04 12:28:56 -0400

    ci: build SDL first

--- ci/macos-11.sh
+++ ci/macos-11.sh
@@ -6,6 +6,9 @@ tar -xf zig-macos-x86_64-0.11.0.tar.xz -C /tmp
 sudo cp -R /tmp/zig-macos-x86_64-0.11.0/* /opt/zig
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install freetype2 harfbuzz ncurses pkg-config
+pushd lib/SDL
+/opt/zig/zig build -Doptimize=ReleaseFast
+popd
 pushd lib/ziglua
 /opt/zig/zig build -Doptimize=ReleaseFast
 popd
